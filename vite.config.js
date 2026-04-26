9cd56c663738a420498a1fafea718a4fbeaaa96a
Preshybelle <isabellaemmanuel04@gmail.com>
2026-04-26 07:49:44 +0100

    clean project structure for Cloudflare deploy

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/MandaBelle-Hairs/',
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+        },
+      },
+    },
+  },
 })
